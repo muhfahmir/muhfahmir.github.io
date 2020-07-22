3209d151b04f88de6a380b15dd2cbbfb19e24d5b
Fahmi Rahmat <fahmi.rahmat07@gmail.com>
2020-07-23 04:29:20 +0700

    initial commit

--- js/api.js
+++ js/api.js
@@ -4,31 +4,70 @@ let urlClasement;
 const tokenApi = 'a97d836f073d4d6d845a49eb6014e7dd';
 
 const fetchApi = (url) =>{
-	return fetch(url,{
-		headers:{
-			'X-Auth-Token' : tokenAPI
-		}
-	})
+  return fetch(url,{
+    headers:{
+      'X-Auth-Token' : tokenApi
+    }
+  })
 }
 
-const getDataBola => (id) =>{
-	urlClasement = `https://api.football-data.org/v2/competitions/${id}/standings`
-	fetchApi(urlClasement).then((response) => {
-		return response.json();
-	}).then((data) => {
-		console.log(data);
-		// console.log(`${data.competition.name},${data.season.startDate},${data.season.endDate},${data.competition.area.name}`)
-		const titleData = `
-		<div class="col-12 my-3">
-	      <div class="card">
-	        <div class="card-body">
-	          <h1 class="card-title">${data.competition.name}</h1>
-	          <p class="card-text">Start Date : ${data.season.startDate}</p>
-	          <p class="card-text">End Date : ${data.season.endDate}</p>
-	          <p class="card-text" style="float: right">Country : ${data.competition.area.name}</p>
-	        </div>
-	      </div>
-	    </div>`
-    document.querySelector("#bodyContent").innerHTML = titleData;
-	})
+const getBgColor = (index) => {
+  const color = ["primary","secondary","success","danger","warning","info","dark"];
+  return color[index];
+}
+
+const getRankColor = (rank) => {
+  const color = ["dark","danger","warning","info"]
+  return color> 3?color[0]:color[rank];
+}
+
+const getDataBola = (id) =>{
+  urlClasement = `https://api.football-data.org/v2/competitions/${id}/standings`
+  fetchApi(urlClasement).then((response) => {
+    return response.json();
+  }).then((data) => {
+    console.log(data);
+    // console.log(`${data.competition.name},${data.season.startDate},${data.season.endDate},${data.competition.area.name}`)
+    let indexColor = Math.floor(Math.random()*7);
+    let bgColor = getBgColor(indexColor);
+    const titleData = `
+    <div class="col-md-12 my-3 text-white">">
+        <div class="card titleLeague bg-${bgColor}">
+          <div class="card-body">
+            <h2 class="card-title">${data.competition.name}</h2>
+            <span class="card-text">Start Date : ${data.season.startDate}</span><br>
+            <span class="card-text">End Date : ${data.season.endDate}</span>
+            <span class="countryLeague">Country : ${data.competition.area.name}</span>
+          </div>
+        </div>
+    </div>`
+    document.querySelector("#contentHome").innerHTML = titleData;
+
+    let clubsData = '';
+    data.standings[0].table.forEach((club) => {
+      let colorRank = getRankColor(club.position);
+      clubsData+=`
+      <div class="col-md-12 col-sm-12 my-3 col-xs-12">
+        <div class="card cardTeam" >
+          <div class="row no-gutters">
+            <div class="col-md-2 col-sm-3 p-3 col-xs-12 text-center">
+              <img src="${club.team.crestUrl}" class="card-img imgTeam"alt="${club.team.name}">
+            </div>
+            <div class="col-md-10 col-sm-9 col-xs-12">
+              <div class="card-body">
+                <span class="card-title nameTeam">${club.team.name} -<b> ${club.points} pts</b></span>
+                <span class="card-text rankTeam text-${colorRank}">#${club.position}</span><br>
+                <span class="statusTeam bg-success text-white"><b>Won : ${club.won}</b></span>
+                <span class="statusTeam bg-warning text-white"><b>Draw : ${club.draw}</b></span>
+                <span class="statusTeam bg-danger text-white"><b>Lost : ${club.lost}</b></span><br>
+                <span class="card-text "><b><i class="far fa-clock"></i> Jumlah Pertandingan : ${club.playedGames}</b></span><br>
+                <span class="card-text "><b><i class="far fa-futbol"></i> Goals : ${club.goalsFor}</b></span><br>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+      document.querySelector("#contentTim").innerHTML=clubsData;
+    })
+  })
 }
--- js/main.js
+++ js/main.js
@@ -1,87 +1,39 @@
-let urlTeam;
-let urlClasement;
+document.addEventListener("DOMContentLoaded",function(){
+	loadNav()
+	function loadNav(){
+		let xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.querySelectorAll(".navbar-nav").forEach((elemen) => {
+					elemen.innerHTML = xhttp.responseText;
+				});
+				
+				document.querySelector(".navbar-brand").addEventListener("click", () => {
+					loadContent('2001');
+				});
 
-const tokenApi = 'a97d836f073d4d6d845a49eb6014e7dd';
-
-const fetchApi = (url) =>{
-  return fetch(url,{
-    headers:{
-      'X-Auth-Token' : tokenApi
-    }
-  })
-}
-
-const getDataBola = (id) =>{
-  urlClasement = `https://api.football-data.org/v2/competitions/${id}/standings`
-  fetchApi(urlClasement).then((response) => {
-    return response.json();
-  }).then((data) => {
-    console.log(data);
-    // console.log(`${data.competition.name},${data.season.startDate},${data.season.endDate},${data.competition.area.name}`)
-    const titleData = `
-    <div class="col-12 my-3">
-        <div class="card">
-          <div class="card-body">
-            <h1 class="card-title">${data.competition.name}</h1>
-            <p class="card-text">Start Date : ${data.season.startDate}</p>
-            <p class="card-text">End Date : ${data.season.endDate}</p>
-            <p class="card-text" style="float: right">Country : ${data.competition.area.name}</p>
-          </div>
-        </div>
-      </div>
-      <div id="contentTim">
-      </div>`
-    document.querySelector("#bodyContent").innerHTML = titleData;
-
-    let clubsData = '';
-    data.standings[0].table.forEach((club) => {
-      clubsData+=`
-      <div class="col-12 my-3">
-        <div class="card mb-3 p-3" >
-          <div class="row no-gutters">
-            <div class="col-md-2">
-              <img src="${club.team.crestUrl}" class="card-img logo-team" style="width: 150px" alt="${club.team.name}">
-            </div>
-            <div class="col-md-10">
-              <div class="card-body">
-                <h3 class="card-title">${club.team.name} - ${club.points} pts</h3>
-                <span class="card-text">#${club.team.position}</span>
-                <span class="card-text">Won : ${club.won}</span>
-                <span class="card-text">Draw : ${club.draw}</span>
-                <span class="card-text">Lost : ${club.lost}</span>
-                <br>
-                <button type="button">Add to Favorites</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>`
-      document.querySelector("#contentTim").innerHTML=clubsData;
-    })
-
-    // document.querySelectorAll(".nav-link a").forEach((elm) => {
-    //     elm.addEventListener("click",(event) => {
-    //       page = elm.getAttribute("id");
-    //       console.log(page);
-    //     })
-    //   })
-  })
-}
-
-// function getContent(fragmentId){
-//   return fragmentId;
-// }
-
-function loadContent(){
-  let contentDiv = document.getElementById("bodyContent"),
-      fragmentId = location.hash.substr(1);
-  getDataBola(fragmentId);
-}
-
-if(!location.hash) {
-  location.hash = "#2001";
-}
-
-loadContent();
-
-window.addEventListener("hashchange", loadContent)
+				document.querySelectorAll(".navbar-nav a").forEach((elemen) =>{
+					elemen.addEventListener("click", (event) => {
+						let pageId = elemen.getAttribute("id");
+						loadContent(pageId);
+					});
+				});
+			}
+		};
+		xhttp.open("GET", `navbar.html`, true);
+		xhttp.send();
+	}
+	let loadContent = (pageId) =>{
+		let xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				let content = document.querySelector("#bodyContent");
+				content.innerHTML = this.responseText;
+				getDataBola(pageId);
+			}
+		};
+		xhttp.open("GET", `pages/content.html`, true);
+		xhttp.send();
+	}
+	loadContent('2001');
+});
